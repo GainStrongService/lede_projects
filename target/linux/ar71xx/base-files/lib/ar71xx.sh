f8d6435c894f5250b3769fcabedf16447b17d65d
nolan <nolan@ioes.cn>
2018-07-26 18:49:33 +0800

    CPE5GD02

--- target/linux/ar71xx/base-files/lib/ar71xx.sh
+++ target/linux/ar71xx/base-files/lib/ar71xx.sh
@@ -1469,6 +1469,9 @@ ar71xx_board_detect() {
 	*"ZCN-1523H-5")
 		name="zcn-1523h-5"
 		;;
+	*"CPE5GD02 board")
+		name="cpe5gd02"
+		;;
 	esac
 
 	[ -z "$AR71XX_MODEL" ] && [ "${machine:0:8}" = 'TP-LINK ' ] && \
