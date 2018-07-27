714abf69100ec100f48f834c95d17565dcd19739
nolan <nolan@ioes.cn>
2018-07-27 09:30:03 +0800

    oolitev5r

--- target/linux/ar71xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ar71xx/base-files/lib/upgrade/platform.sh
@@ -483,7 +483,8 @@ platform_check_image() {
 	tl-wr941nd|\
 	tl-wr941nd-v5|\
 	tl-wr941nd-v6|\
-	wifi-pineapple-nano)
+	wifi-pineapple-nano|\
+	oolite-v5r)
 		local magic_ver="0100"
 
 		case "$board" in
